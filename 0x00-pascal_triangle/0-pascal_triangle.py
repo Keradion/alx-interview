e45eec9ed85c06ef596695085f5386974444ee13
Daniel Berihun <danielshitaye10@gmail.com>
2024-10-06 15:34:48 +0000

    Py style

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -14,6 +14,8 @@ def pascal_triangle(n):
     pascals_list = [[1], [1, 1]]
     list_to_append = []  # Denotes the nested list
     sum = 0  # Hold sum of list elements
+    num1 = 0
+    num2 = 0
 
     if n <= 0:
         return []
@@ -26,14 +28,14 @@ def pascal_triangle(n):
     while row_counter <= n - 2:
         list_to_append.append(1)  # Insert 1 at the beginning of the list
         while list_column_counter <= row_counter - 1:
-            sum = pascals_list[row_counter][list_column_counter] +
-            pascals_list[row_counter][list_column_counter + 1]
+            num1 = pascals_list[row_counter][list_column_counter]
+            num2 = pascals_list[row_counter][list_column_counter + 1]
+            sum = num1 + num2
             list_to_append.append(sum)
             list_column_counter += 1
         list_to_append.append(1)  # Insert 1 at the end of the list
         pascals_list.append(list_to_append)  # Append the new row list
         list_to_append = []  # Reset for each new iteration
-        sum = 0
         row_counter += 1
         list_column_counter = 0
     return pascals_list
