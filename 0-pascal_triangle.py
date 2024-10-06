fcc67a3cba082cf9dbf93c4eff6aa2d9f92c784a
Daniel Berihun <danielshitaye10@gmail.com>
2024-10-06 15:00:22 +0000

    pascals triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+   Returns a list of lists of integers representing the Pascal’s triangle of n:
+"""
+def pascal_triangle(n):
+    pascals_list = [[1], [1,1]]
+    list_to_append = [] #Denotes the nested list 
+    sum = 0 #Hold sum of list elements
+
+    if n <= 0:
+        return []
+    if n==1:
+        return [[1]]
+    
+    row_counter = 1 #working from row 1
+    list_column_counter = 0 #to iterate in each column of a list in a given row during summation
+    
+    while row_counter <= n - 2:
+        list_to_append.append(1) #Insert 1 at the begin of the list
+        while list_column_counter <= row_counter - 1 :
+            sum = pascals_list[row_counter][list_column_counter] + pascals_list[row_counter][list_column_counter + 1]
+            list_to_append.append(sum)
+            list_column_counter +=1 
+            list_to_append.append(1) #Insert 1 at the end of the list
+            pascals_list.append(list_to_append) #Append the new row list 
+            list_to_append = [] #Reset for each new iteration
+            sum = 0
+            row_counter +=1
+            list_column_counter = 0
+    return pascals_list
